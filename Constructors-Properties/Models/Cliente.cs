56ba0e383862474bb9c15846c260db1bcc36c804
Marcus Lara <marcus.lara@hotmail.com>
2024-01-22 16:04:06 -0300

    create prop and methods

---
+++ Constructors-Properties/Models/Cliente.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Constructors_Properties.Models
+{
+    public class Cliente
+    {
+        public int NumeroConta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; set; }
+
+        public Cliente(int numeroConta, string nome)
+        {
+            Saldo = 0;
+        }
+
+        public Cliente(int numeroConta, string nome, double saldo)
+        {
+           NumeroConta = numeroConta;
+           Nome = nome;
+           Saldo = saldo;
+        }
+
+        public void Depositar(double deposito) 
+        {
+            Saldo += deposito;
+            Console.WriteLine("p");
+        }
+
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroConta}, Titular: {Nome}, Saldo: {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+
+}
--- Constructors-Properties/Program.cs
+++ Constructors-Properties/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Constructors_Properties.Models;
+using System.Globalization;
+
+Console.Write("Entre o número da conta: ");
+int conta = int.Parse(Console.ReadLine());
+
+Console.Write("Entre o titular da conta: ");
+string nome = Console.ReadLine();
+
+string operacao;
+
+
+do
+{
+    Console.Write("Haverá depósito inicial (s/n)? ");
+    operacao = Console.ReadLine().ToLower();
+
+}while(operacao != "s" && operacao != "n");
+
+if(operacao == "s")
+{
+    Cliente c1 = new Cliente(conta, nome);
+}
